ac5b6933edf14ad0496bb691a435ea59156b6c0c
Andy L <andyqly@hotmail.com>
2019-11-01 11:46:18 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -14,3 +14,8 @@ function addElementToEndOfArray(array, element){
 [...array, element,];
 return array
 }
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
