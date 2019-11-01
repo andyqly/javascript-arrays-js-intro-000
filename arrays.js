9fb7c4909d275c08de62af5d21f5db4201268430
Andy L <andyqly@hotmail.com>
2019-11-01 11:13:22 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,6 +2,5 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(purse, "coin"){
   var purse = ["coin", ]
-  purse.push("coin")
-  addElementToBeginningOfArray
+
 }
